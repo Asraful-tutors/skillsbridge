0b190075521d5874f01ae5fe10d704d51de41715
Asraful-tutors <asraful.islam@tutors.es>
2024-03-29 15:56:04 +0600

    updated smtp

--- lib/backend/mail.ts
+++ lib/backend/mail.ts
@@ -1,16 +1,32 @@
 // @ts-ignore
+import nodemailer from "nodemailer";
 
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+// Create a nodemailer transporter
+const transporter = nodemailer.createTransport({
+  host: "smtp",
+  service: "gmail",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "skillsbridgeinfo@gmail.com",
+    pass: "deva nrkm yres izqz",
+  },
+});
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `https://app.skillsbridge.eu/password?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: email,
-    subject: "Reset your password",
-    html: `<p>Click <a href="${resetLink}">here</a> to reset your password</p>`,
-  });
+  try {
+    // Send mail with defined transport object
+    await transporter.sendMail({
+      from: "skillsbridgeinfo@gmail.com", // sender address
+      to: email, // list of receivers
+      subject: "Reset your password", // Subject line
+      html: `<p>Click <a href="${resetLink}">here</a> to reset your password</p>`, // html body
+    });
+
+    console.log("Password reset email sent successfully.");
+  } catch (error) {
+    console.error("Error occurred while sending password reset email:", error);
+  }
 };
